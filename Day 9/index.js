cd0ce1275a7af0838332579d454c32cca52d8123
Adebesin-Cell <adebesintolulope80@gmail.com>
2022-04-14 21:25:26 +0100

    added task

--- Day 9/index.js
+++ Day 9/index.js
@@ -1,19 +1,23 @@
 const generatePascalTriangle = function (numRows) {
-  if (numRows === 0) return [];
-  if (numRows === 1) return [[1]];
-  let result = [];
-  for (let row = 1; row <= numRows; row++) {
-    let arr = [];
-    for (let col = 0; col < row; col++) {
-      if (col === 0 || col === row - 1) {
-        arr.push(1);
-      } else {
-        arr.push(result[row - 2][col - 1] + result[row - 2][col]);
+  if (typeof numRows !== "number") return "input is not a number";
+  else {
+    if (numRows === 0) return [];
+    if (numRows === 1) return [[1]];
+    let result = [];
+    for (let row = 1; row <= numRows; row++) {
+      let arr = [];
+      for (let col = 0; col < row; col++) {
+        if (col === 0 || col === row - 1) {
+          arr.push(1);
+        } else {
+          arr.push(result[row - 2][col - 1] + result[row - 2][col]);
+        }
       }
+      result.push(arr);
     }
-    result.push(arr);
+    return result;
   }
-  return result;
 };
 
+console.log(generatePascalTriangle("6"));
 console.log(generatePascalTriangle(6));
